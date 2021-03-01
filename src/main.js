b9a2e4d28f53ef3696184160a2fd886dc7eefabd
jpneey <burato348@gmail.com>
2021-03-02 07:38:30 +0800

    Added Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import gA from 'vue-analytics'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router, gA, { id: 'UA-160195176-1', checkDuplicatedScript: true }).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import gA from 'vue-analytics'
+
 
 const routes = [
   {
